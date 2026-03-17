eb27f2ae9776797057218e0126efabb3556995cd
Jusuf Suljic <jusuf.uni@gmail.com>
2026-03-17 12:30:29 +0100

    pistol1 - 28/03/26

--- src/app/obuka/ObukaContent.tsx
+++ src/app/obuka/ObukaContent.tsx
@@ -36,7 +36,7 @@ const trainings = [
         </ul>
 
         <p>Lokacija: Sportsko strelište Žuč</p>
-        <p>Datum: 14.03.2026. (Subota)</p>
+        <p>Datum: 28.03.2026. (Subota)</p>
         <p>Trajanje: 60 Minuta</p>
       </>
     ),

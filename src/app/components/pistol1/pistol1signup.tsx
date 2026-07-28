7ebc43050422c107dadc30c4e9cf6de6cd0ba7d0
Jusuf Suljic <jusuf.uni@gmail.com>
2026-07-28 10:12:18 +0200

    pistol1 - 08/08/26

--- src/app/components/pistol1/pistol1signup.tsx
+++ src/app/components/pistol1/pistol1signup.tsx
@@ -61,7 +61,7 @@ export default function Pistol1Signup({ groups }: { groups: Group[] }) {
     return (
         <SignUpPageContainer>
             <SignUpForm onSubmit={handleSubmit}>
-                <h1>REGISTRACIJA ZA DELTA - PISTOL LEVEL 1 (25.07.2026.)</h1>
+                <h1>REGISTRACIJA ZA DELTA - PISTOL LEVEL 1 (08.08.2026.)</h1>
 
                 <CustomSignUpInputContainer>
                     <label>Ime</label>
